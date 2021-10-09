e235e2dd864aec29715cfabf5ed6c8ecd0e60287
TheSonicMaster <supdroidgamingdivision@gmail.com>
2021-10-09 17:25:55 +0100

    Fix db-5.3.28

--- build-system.sh
+++ build-system.sh
@@ -885,7 +885,7 @@ cd build_unix
 ../dist/configure --prefix=/usr --enable-compat185 --enable-dbm --disable-static --enable-cxx
 make
 make install
-chown -R root:root /usr/bin/db_* /usr/include/db{,_185,_cxx}.h /usr/lib/libdb*.{so,la} /usr/share/doc/db-5.3.28
+chown -R root:root /usr/bin/db_* /usr/include/db{,_185,_cxx}.h /usr/lib/libdb*.{so,la}
 cd ../..
 rm -rf db-5.3.28
 # LMDB.
