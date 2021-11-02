63bcf84e4cd386b58dee5384f1c3aeec92aa1009
TheSonicMaster <supdroidgamingdivision@gmail.com>
2021-11-02 17:49:22 +0000

    Fix Net-SNMP random install failure

--- build-system.sh
+++ build-system.sh
@@ -3996,7 +3996,7 @@ tar -xf net-snmp-5.9.1.tar.xz
 cd net-snmp-5.9.1
 ./configure --prefix=/usr --sysconfdir=/etc --mandir=/usr/share/man --enable-ucd-snmp-compatibility --enable-ipv6 --with-python-modules --with-default-snmp-version="3" --with-sys-contact="root@localhost" --with-sys-location="Unknown" --with-logfile="/var/log/snmpd.log" --with-mib-modules="host misc/ipfwacc ucd-snmp/diskio tunnel ucd-snmp/dlmod ucd-snmp/lmsensorsMib" --with-persistent-directory="/var/net-snmp"
 make NETSNMP_DONT_CHECK_VERSION=1
-make INSTALLDIRS=vendor install
+make -j1 INSTALLDIRS=vendor install
 install -m644 systemd-units/snmpd.service /usr/lib/systemd/system/snmpd.service
 install -m644 systemd-units/snmptrapd.service /usr/lib/systemd/system/snmptrapd.service
 for i in libnetsnmp libnetsnmpmibs libsnmp libnetsnmphelpers libnetsnmptrapd libnetsnmpagent; do
